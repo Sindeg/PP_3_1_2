3cab0ecd33f39102c7f18513d2bdb59dea1cb8a0
Evgeniy <ifzy12@gmail.com>
2023-10-27 19:53:44 +0300

    style(userDao): add spaces between methods

--- src/main/java/ru/kata/springboot/dao/UserDao.java
+++ src/main/java/ru/kata/springboot/dao/UserDao.java
@@ -7,9 +7,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserDao {
+
     void save(User user);
+
     List<User> findAll();
+
     Optional<User> findById(Long id);
+
     void updateUser(User user);
+
     void deleteById(Long id);
 }

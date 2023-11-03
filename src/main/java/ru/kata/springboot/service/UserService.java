f427c3e4cd9706adde5fb4bb27eb856f3e9f04c9
Evgeniy <ifzy12@gmail.com>
2023-11-03 16:40:41 +0300

    refactor: add interfaces to service

--- src/main/java/ru/kata/springboot/service/UserService.java
+++ src/main/java/ru/kata/springboot/service/UserService.java
@@ -1,53 +1,21 @@
 package ru.kata.springboot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.springboot.model.User;
-import ru.kata.springboot.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    User save(User user);
 
-    @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    List<User> findAll();
 
-    @Transactional
-    public User save(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
+    Optional<User> findById(Long id);
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
+    void updateUser(User user);
 
-    public Optional<User> findById(Long id) {
-        return userRepository.findById(id);
-    }
+    void deleteById(Long id);
 
-    @Transactional
-    public void updateUser(User user) {
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        userRepository.deleteById(id);
-    }
-
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
+    Optional<User> findByEmail(String email);
 }

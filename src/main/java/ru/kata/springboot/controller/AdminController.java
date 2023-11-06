d184c09ef30d235a13380780ad94098e5fad0dfa
Evgeniy <ifzy12@gmail.com>
2023-11-06 14:11:43 +0300

    add sidebar, modal windows for deleting and editing user

--- src/main/java/ru/kata/springboot/controller/AdminController.java
+++ src/main/java/ru/kata/springboot/controller/AdminController.java
@@ -1,7 +1,6 @@
 package ru.kata.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,11 +11,14 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.kata.springboot.model.Role;
 import ru.kata.springboot.model.User;
 import ru.kata.springboot.service.RoleService;
 import ru.kata.springboot.service.UserService;
 
 import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -26,45 +28,38 @@ public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
 
-    private final PasswordEncoder passwordEncoder;
-
     @Autowired
     public AdminController(UserService userService,
-                           RoleService roleService,
-                           PasswordEncoder passwordEncoder) {
+                           RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/users")
-    public String getUsersList(Model model) {
+    public String getUsersList(Principal principal, Model model) {
         model.addAttribute("users", userService.findAll());
+
+        addAttributesToMainPage(model, principal);
+
         return "/admin/user-list";
     }
 
-    @GetMapping("/users/new")
-    public String createUserForm(@ModelAttribute("user") User user, Model model) {
-        model.addAttribute("listRoles", roleService.findAll());
-
-        return "/admin/user-create";
-    }
-
     @PostMapping("/users")
-    public String createUser(@ModelAttribute("user") @Valid User user,
-                             BindingResult bindingResult, Model model) {
-        Optional<User> userByEmail = userService.findByEmail(user.getEmail());
+    public String createUser(@ModelAttribute("newUser") @Valid User newUser,
+                             BindingResult bindingResult, Model model, Principal principal) {
+        Optional<User> userByEmail = userService.findByEmail(newUser.getEmail());
         if (userByEmail.isPresent()) {
             bindingResult.rejectValue("email", "error.email",
                     "This email is already in use");
         }
 
         if (bindingResult.hasErrors()) {
-            model.addAttribute("listRoles", roleService.findAll());
-            return "/admin/user-create";
+            model.addAttribute("hasErrors", true);
+            addAttributesToMainPage(model, principal);
+            return "/admin/user-list";
         }
 
-        this.userService.save(user);
+        this.userService.save(newUser);
         return "redirect:/admin/users/";
     }
 
@@ -82,29 +77,58 @@ public class AdminController {
     }
 
     @PatchMapping("/users/edit")
-    public String editUser(@ModelAttribute("user") @Valid User updatedUser,
-                           BindingResult bindingResult, Model model) {
-        Optional<User> userByEmail = userService.findByEmail(updatedUser.getEmail());
-        if (userByEmail.isPresent() && (!userByEmail.get().getId().equals(updatedUser.getId()))) {
+    public String editUser(@ModelAttribute("updatingUser") @Valid User updatingUser,
+                           BindingResult bindingResult, Model model, Principal principal) {
+        Optional<User> userByEmail = userService.findByEmail(updatingUser.getEmail());
+        if (userByEmail.isPresent() && (!userByEmail.get().getId().equals(updatingUser.getId()))) {
             bindingResult.rejectValue("email", "error.email",
                     "This email is already in use");
         }
 
         if (bindingResult.hasErrors()) {
-            model.addAttribute("listRoles", roleService.findAll());
-            return "/admin/edit-user";
+            addAttributesToMainPage(model, principal);
+            model.addAttribute("editUserError", true);
+            return "/admin/user-list";
         }
 
-        userService.updateUser(updatedUser);
+        userService.updateUser(updatingUser);
         return "redirect:/admin/users";
     }
 
     @DeleteMapping("/users/delete")
-    public String deleteUser(@RequestParam("id") Long id) {
+    public String deleteUser(@ModelAttribute("deletingUser") User user) {
+        Long id = user.getId();
         if (userService.findById(id).isPresent()) {
             userService.deleteById(id);
         }
         return "redirect:/admin/users";
+    }
+
+    private void addAttributesToMainPage(Model model, Principal principal) {
+        User user = userService.findByEmail(principal.getName()).orElse(new User());
+
+        List<String> roles = user.getRoles().stream()
+                .map(Role::getName)
+                .map(role -> role.split("_")[1])
+                .toList();
+
+        model.addAttribute("authUser", user);
+        model.addAttribute("userRoles", roles);
+
+        if (!model.containsAttribute("updatingUser")) {
+            model.addAttribute("updatingUser", new User());
+        }
+
+        if (!model.containsAttribute("newUser")) {
+            model.addAttribute("newUser", new User());
+        }
+
+        if (!model.containsAttribute("deletingUser")) {
+            model.addAttribute("deletingUser", new User());
+        }
+
+        model.addAttribute("listRoles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
     }
 }
 
--- src/main/java/ru/kata/springboot/controller/UserController.java
+++ src/main/java/ru/kata/springboot/controller/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.springboot.model.Role;
 import ru.kata.springboot.model.User;
 import ru.kata.springboot.service.UserService;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -24,8 +26,13 @@ public class UserController {
     @GetMapping
     public String getUserInfo(Principal principal, Model model) {
         User user = userService.findByEmail(principal.getName()).orElse(new User());
-        model.addAttribute("user", user);
+        List<String> roles = user.getRoles().stream()
+                .map(Role::getName)
+                .map(role -> role.split("_")[1])
+                .toList();
 
+        model.addAttribute("authUser", user);
+        model.addAttribute("userRoles", roles);
         return "/user/user-info";
     }
 }
